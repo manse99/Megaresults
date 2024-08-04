a93c578193dbc74af5328fe7f267248bde9ea426
manse99 <40325629+manse99@users.noreply.github.com>
2024-08-04 19:03:08 -0400

    more javascript edits

--- lottery/app.js
+++ lottery/app.js
@@ -1,10 +1,10 @@
 //variables
 let no1 = document.querySelector('no1');
-let no1 = document.querySelector('no2');
-let no1 = document.querySelector('no3');
-let no1 = document.querySelector('no4');
-let no1 = document.querySelector('no5');
-let no1 = document.querySelector('no6');
+let no2 = document.querySelector('no2');
+let no3 = document.querySelector('no3');
+let no4 = document.querySelector('no4');
+let no5 = document.querySelector('no5');
+let no6 = document.querySelector('no6');
 let btn = document.querySelector('.btn');
 
 
