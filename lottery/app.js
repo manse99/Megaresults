5b7de8d95e936298d4c643fb78d460da4087172c
manse99 <40325629+manse99@users.noreply.github.com>
2024-08-04 19:00:15 -0400

    edit updates

---
+++ lottery/app.js
@@ -0,0 +1,24 @@
+//variables
+let no1 = document.querySelector('no1');
+let no1 = document.querySelector('no2');
+let no1 = document.querySelector('no3');
+let no1 = document.querySelector('no4');
+let no1 = document.querySelector('no5');
+let no1 = document.querySelector('no6');
+let btn = document.querySelector('.btn');
+
+
+btn.addEventListener('click', function(){
+    const list = [];
+    for (let i = 0; i < 6; i++){
+        let number = Math.floor(Math.random() * 61);
+        list.push(number);
+    }
+
+    no1.innerHTML = list[0];
+    no1.innerHTML = list[1];
+    no2.innerHTML = list[2];
+    no3.innerHTML = list[3];
+    no4.innerHTML = list[4];
+    no5.innerHTML = list[5];
+})
